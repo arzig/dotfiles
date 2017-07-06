5c411ce54dfb18658739184f7e42fc8962dff51f
Ryan Zeigler <zeiglerr@gmail.com>
2017-07-06 07:57:51 -0500

    Add hyper config.

--- dotfiles.zsh
+++ dotfiles.zsh
@@ -4,4 +4,5 @@
   ./apps.zsh && \
   ./atom.zsh && \
   ./tools.zsh && \
-  ./node.zsh
+  ./node.zsh && \
+  ./hyper.zsh
---
+++ hyper.zsh
@@ -0,0 +1,12 @@
+#!/usr/bin/env zsh
+
+brew cask install hyper
+npm install -g hpm-cli
+
+hpm install hyper-dracula
+hpm install hyperterm-alternatescroll
+hpm install hyperterm-paste
+hpm install hyperterm-tabs
+hpm install hyperlinks
+hpm install hyper-copycat
+hpm install hyper-search
