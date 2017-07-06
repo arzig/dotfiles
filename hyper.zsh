c9a79683c9981c84a9a6dcd3ff0825770178fa66
Ryan Zeigler <zeiglerr@gmail.com>
2017-07-06 15:22:17 -0500

    Change hyper plugins.

--- hyper.zsh
+++ hyper.zsh
@@ -6,6 +6,7 @@ npm install -g hpm-cli
 hpm install hyper-dracula
 hpm install hyperterm-alternatescroll
 hpm install hyperterm-paste
-hpm install hyperterm-tabs
 hpm install hyperlinks
 hpm install hyper-search
+hpm install hyper-alt-click
+hpm install hyperterm-tabs
