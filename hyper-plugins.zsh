44e147960a00b6206de79c7d8abd809f466ae58a
Ryan Zeigler <zeiglerr@gmail.com>
2017-07-06 15:24:50 -0500

    hpm doesn't seem to work outside hyper.

---
+++ hyper-plugins.zsh
@@ -0,0 +1,9 @@
+#!/usr/bin/env zsh
+
+hpm install hyper-dracula
+hpm install hyperterm-alternatescroll
+hpm install hyperterm-paste
+hpm install hyperlinks
+hpm install hyper-search
+hpm install hyper-alt-click
+hpm install hyperterm-tabs
--- hyper.zsh
+++ hyper.zsh
@@ -2,11 +2,3 @@
 
 brew cask install hyper
 npm install -g hpm-cli
-
-hpm install hyper-dracula
-hpm install hyperterm-alternatescroll
-hpm install hyperterm-paste
-hpm install hyperlinks
-hpm install hyper-search
-hpm install hyper-alt-click
-hpm install hyperterm-tabs
